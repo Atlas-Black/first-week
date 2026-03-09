3f725cbc5777ca4774d030c3fa45860f0fafe354
Atlas-Black <2103177529@qq.com>
2026-03-09 16:08:37 +0800

    chore: format lastCheckTime json output

--- src/main/java/com/yiguan/firstweek/model/Device.java
+++ src/main/java/com/yiguan/firstweek/model/Device.java
@@ -1,17 +1,20 @@
 package com.yiguan.firstweek.model;
 
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class Device {
     private Long deviceId;
     private String deviceName;
     private String deviceType;     //投影仪
     private Integer status;     //0 = 正常； 1 = 故障 ；Integer是Int的包装类型，对象类型；可以是null
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime lastCheckTime;    //最终要“返回标准 JSON”，时间字段如果用 LocalDateTime，
                                             // 框架可以更自然地处理它（序列化时有标准格式），你也更容易在后续扩展
                                             //结果的时间戳，这个状态是在什么时候更新的
                                             //最近一次的检查时间
-                                            public Device(){}
+
+    public Device(){}
     public Long getDeviceId(){
         return deviceId;
     }
